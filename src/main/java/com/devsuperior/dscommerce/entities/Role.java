d5f630a0d58c8ee6330a2b2d8ba498eebeda973d
gabri-17 <ga170120@gmail.com>
2026-07-16 22:16:16 -0300

    UserDetails, GrantedAuthority, not tested

--- src/main/java/com/devsuperior/dscommerce/entities/Role.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Role.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscommerce.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.HashSet;
 import java.util.Objects;
@@ -8,7 +9,7 @@ import java.util.Set;
 
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role  implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,6 +39,8 @@ public class Role {
         this.id = id;
     }
 
+    // Confirmação da implementação dessa interface com este metodo
+    @Override
     public String getAuthority() {
         return authority;
     }
